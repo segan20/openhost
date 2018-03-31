7b46016d42774ef1cb9d508350d937ea37ab9d83
segan20 <37962709+segan20@users.noreply.github.com>
2018-03-31 14:50:08 -0400

    Mini Openload Basico Gratis

---
+++ miniopen3.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+ <html>
+ <head>
+ <meta name="robots" content="noindex,nofollow"/>
+<style type="text/css">
+html, body, iframe{width:100%;height:100%; margin:0;padding:0}
+</style>
+</head>
+<body>
+
+<iframe src="<?php echo str_replace(array("http://www.nowvideo.to/video/", "https://openload.co/f/", "http://idowatch.net/", "http://waaw.tv/watch_video.php?v=", "http://streamin.to/"), array("http://embed.nowvideo.to/embed/?v=", "https://openload.co/embed/", "http://idowatch.net/embed-", "http://hqq.tv/player/embed_player.php?vid=", "http://streamin.to/embed-"),$_GET["url"]);?>" frameborder="0" id="player" scrolling="no" allowtransparency="true" allowfullscreen="true"></iframe>
+</body>
+</html> 
